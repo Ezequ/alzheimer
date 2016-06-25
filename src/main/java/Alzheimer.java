a509ebe5905c3fd851ca97427c4f21f2c817d67f
Ezequiel <ezequielmreyes@gmail.com>
2016-06-25 19:19:34 -0300

    Added ksessions

--- src/main/java/Alzheimer.java
+++ src/main/java/Alzheimer.java
@@ -2,9 +2,16 @@
  * Created by ezequiel on 25/06/16.
  */
 import com.opencsv.CSVReader;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Alzheimer {
     public static final void main(final String[] args) throws IOException {
         processFile();
@@ -14,14 +21,29 @@ public class Alzheimer {
         String fileName = "data.csv";
         CSVReader reader = new CSVReader(new FileReader(fileName));
         String[] nextLine;
+        ArrayList<Paciente> pacients = new ArrayList<Paciente>();
         while ((nextLine = reader.readNext()) != null) {
             if (nextLine != null) {
                 Paciente pac = Paciente.createPacient(nextLine);
-                System.out.print(nextLine);
+                pacients.add(pac);
             }
         }
+        processPacients(pacients);
     }
 
+    private static void processPacients(ArrayList<Paciente> pacients) {
+        KieServices ks = KieServices.Factory.get();
+        // From the kie services, a container is created from the classpath
+        KieContainer kc = ks.getKieClasspathContainer();
+        KieSession ksession = kc.newKieSession("AlzheimerKS");
+        ksession.setGlobal( "list",
+                new ArrayList<Object>() );
+        for( Paciente pacient : pacients) {
+            ksession.insert(pacient);
+        }
+        ksession.fireAllRules();
+        ksession.dispose();
+    }
 
 
 }
